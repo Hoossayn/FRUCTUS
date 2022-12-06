01bd72d8111c38878422026856ba9036590b54f3
hoossayn <abdulhakeemhusseni@gmail.com>
2022-12-06 17:30:34 +0100

    Initial Commit

---
+++ FRUCTUS/FRUCTUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FRUCTUSApp.swift
+//  FRUCTUS
+//
+//  Created by hoossayn on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct FRUCTUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
